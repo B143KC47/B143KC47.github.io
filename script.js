8a5c177485fda5073430fe16d8fd10b5031d5322
BlackCat <ltu46166@gmail.com>
2024-11-13 18:36:58 +0800

    Updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const currentYear = new Date().getFullYear();
-    document.getElementById('year').textContent = `©2023-${currentYear} Powered by BlackCat`;
+    document.getElementById('year').textContent = `©2023-${currentYear} Powered by KO Ho Tin`;
 });
