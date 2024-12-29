7d648c85142f1b541355b6ba02e60e7e5f4f0a66
KO Ho Tin <tiangong1337@outlook.com>
2024-12-29 22:30:09 +0800

    添加移动端导航显隐控制及样式调整

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const currentYear = new Date().getFullYear();
     document.getElementById('year').textContent = `©2023-${currentYear} Powered by KO Ho Tin`;
 });
+
+// 在移动端可选添加下拉连接的显隐控制
+function toggleMobileNav() {
+    const nav = document.querySelector('.navbar');
+    if (nav.style.display === 'none') {
+        nav.style.display = 'flex';
+    } else {
+        nav.style.display = 'none';
+    }
+}
